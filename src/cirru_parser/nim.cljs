94b222f20aa3a21dbf941a675d0837a5ed103529
jiyinyiyong <jiyinyiyong@gmail.com>
2019-12-27 00:50:05 +0800

    less memory usage in tranforming nim result to Clojure; alpha release

--- src/cirru_parser/nim.cljs
+++ src/cirru_parser/nim.cljs
@@ -2,11 +2,22 @@
 (ns cirru-parser.nim
   (:require ["@cirru/parser.nim" :refer [parseCirru]] [clojure.string :as string]))
 
+(defn merge-chars [xs]
+  (if (nil? xs)
+    ""
+    (let [*result (atom "")]
+      (.forEach xs (fn [x] (swap! *result str (js/String.fromCharCode x))))
+      @*result)))
+
 (defn transform-data [tree]
   (if (= (.-kind tree) 1)
-    (let [expr-list (-> tree .-list)]
-      (if (nil? expr-list) (js/Array.) (-> expr-list (.map transform-data) (js->clj))))
-    (-> tree .-text (.map (fn [n] (js/String.fromCharCode n))) (.join ""))))
+    (let [expr-list (.-list tree)]
+      (if (nil? expr-list)
+        []
+        (let [*result (atom [])]
+          (.forEach expr-list (fn [child] (swap! *result conj (transform-data child))))
+          @*result)))
+    (merge-chars (.-text tree))))
 
 (defn parse [code]
   (let [js-tree (parseCirru (-> code (.split "") (.map (fn [c] (.charCodeAt c 0)))))]
